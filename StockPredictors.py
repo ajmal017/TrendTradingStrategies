b3f84b0e3c35766e8f379cf8b37110c995c3b385
jgdennery1998 <lucidatious@gmail.com>
2020-02-21 12:17:21 -0500

    Corrections to ADX calculator

--- StockPredictors.py
+++ StockPredictors.py
@@ -2,42 +2,39 @@
 Statistical Methods of Predicting Stock Prices
 
 """
+
 ###Imports
 import numpy as np
 from scipy import stats
-
 
 ### Logarithmic Return
 
 def log_get_returns(prices):
   returns = []
-  count = 0
-  for i in enumerate (prices,0):
+  for i in range(prices):
     #print(i,'\n') making sure the loop iterates over the right number of elements
  
-    price_final = prices[count+1]
-    price_initial = prices[count]
+    price_final = prices[i+1]
+    price_initial = prices[i]
     log_return = np.log(price_final/price_initial) #logreturns; see documents for explanation
     returns.append(log_return)   
     
   #index update for 'count' after operations but before break statement~~ need to stop iteration from going to 12th index because there are n - 1 intervals given n elements in a list, but updating index before operations would skip the first element and ask for a list index out of range
 
-    count += 1
-    if count == len(prices) -1:
+    if i == len(prices) - 2:
       break
   return returns
 
 
 
 ### Simple Linear Regression Predictor (within 1/n * standard deviations, where n is selected by the user)
-def lin_reg_predictor(prices,n): #prices array-like, n > 0
+def lin_reg_predictor(prices,n=1): #prices array-like, n > 0
 
     x = np.linspace(0.0,float(len(prices) - 1), len(prices))
     slope, intercept, r_value, p_value, std_err = stats.linregress(x,prices)
     prediction = slope*(len(prices)) + intercept #extrapolating values stock price for the following day/week/month
 
     #note to self, if regression is provided in terms of days, the x vector will not work since it treats days as evenly spaced; to be dealt with later
-    #could instead have a vector consisting of 
     prediction_upper_bound = prediction + std_err/n
     prediction_lower_bound = prediction - std_err/n #actual value of following stock price should be within range of upper/lower bound; else lin_reg_predictor is not predictive
 
@@ -51,9 +48,9 @@ def lin_reg_predictor(prices,n): #prices array-like, n > 0
 #####Directional Movement List Generator
 def DM(listprice):
     DM_list = []
-    for count in enumerate(listprice,0):
+    for i in range(listprice):
         if count > 0:
-            DM = listprice[count] - listprice[count - 1]
+            DM = listprice[i] - listprice[i - 1]
             DM_list.append(DM)
     return DM_list
 
@@ -63,7 +60,7 @@ def DM(listprice):
 #####True Range
 def TR(listprice1,listprice2): #listprice1,2 have to be the same size.
     TR_list = [] #further, listprice1 should be the high DM, listprice2 should be the low DM (subject to modification)
-    for count in enumerate(listprice1,0):
+    for i in range(listprice1):
         if count > 0:
             Op1 = listprice1[count] - listprice1[count-1]
             Op2 = listprice2[count] - listprice2[count-1]
@@ -128,59 +125,44 @@ def DX_initiator(list1,list2):#lists should be same length, n should be integer
 
 ### the following loop should apply ADX_initiator to first N entries and then iterate for as many N-length periods as the user demands (provided there is enough data)
 
-def CurrentADX(final,list1,list2,period,numLargePeriods):
-  initial = final - period*numLargePeriods*14 #possibly some index issues
+def CurrentADX(final,list1,list2,period,NumLargePeriod):
+  initial = final - NumLargePeriod*period*14 #possibly some index issues
   DM_high = DM(list1)
   DM_low = DM(list2)
-  TR_index = TR(list1,list2)
-  DX = 0
-  i = 0
+  DX_vector = []
+  ADX_vector = []
+  i = initial
+  j = 0 #for dx vector
+  k = 0 #for adx vector
+  N = NumLargePeriod*13
 
   #getting initial ADX
 
-  while i < 14*period - 2:
+  while i < 14*period*NumLargePeriod - 1:#potential indexing issues, to be worked out later
     High_list = []
     Low_list = []
-    Range_list = []
     count = 0
     while count < period:
       High_list.append(DM_high[i + count])
       Low_list.append(DM_low[i + count])
-      Range_list.append(TR_index[i + count])
+      count += 1
       
-    N,DX_i = DX_initiator(High_list,Low_list)
-    i += count + 1 #count is (period - 1) at this point--I need to move period indexes away to calculate ADX
-    DX += DX_i
-     
-  
-  i = initial + 14*period - 1
-  ADX_i = DX // 14.0
-  
-  #the code below is a faulty implementation of adx calculator--to be modified later
-  while i < final - 1:
-    j = i
-    DX = 0
-    while i < j + 14*period - 2:
-      High_list = []
-      Low_list = []
-      Range_list = []
-      count = 0
-      while count < period:
-        High_list.append(DM_high[i + count])
-        Low_list.append(DM_low[i + count])
-        Range_list.append(TR_index[i + count])
+    DX_i = DX_initiator(High_list,Low_list)
+    DX_vector.append(DX_i)#dx vector stuff
+    j += 1
+
+
+    if i == initial + period*14:#double check for indexing
+      currentADX = np.average(DX_vector)
+      ADX_vector.append(currentADX)
+      k += 1
+    elif i > initial + period*14:#doublecheck for indexing
+      prevADX = ADX_vector[int(k) - 1]
+      currentADX = DX_i*(2//(N + 1)) + prevADX(1 - (2//(N + 1))) #N = NumLargePeriods813 could possibly be wrong, but this is a relatively easy fix; more problematic would be if this formula is incorrectly implemented
+      ADX_vector.append(currentADX)
+      k += 1
       
-      DX_update = DX_initiator(High_list,Low_list)
-      DX += DX_update
+    #changing index by three
+    i += count
 
-      i += count + 1 #count is (period - 1) at this point--I need to move period indexes away to calculate ADX
-      
-    ADX_current = DX // 14.0
-
-    current_ADX = ADX_current(2//(numLargePeriods + 1)) - ADX_i*(1 - 2//(numLargePeriods + 1))
-    ADX_i = current_ADX
-
-  return current_ADX
-
-  ### 
-
+  return ADX_vector
