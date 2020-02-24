a6bd12a8ee6fb4371a27d6238909a17f48704fbb
jgdennery1998 <lucidatious@gmail.com>
2020-02-24 17:26:16 -0500

    Simplified after addition of DirectionalIndexTools.py

--- StockPredictors.py
+++ StockPredictors.py
@@ -6,6 +6,8 @@ Statistical Methods of Predicting Stock Prices
 ###Imports
 import numpy as np
 from scipy import stats
+import DirectionalIndexTools as DITools #Note: This assumes that ADX file is in the same directory as the current program.
+
 
 ### Logarithmic Return
 
@@ -41,94 +43,12 @@ def lin_reg_predictor(prices,n=1): #prices array-like, n > 0
     return prediction_upper_bound, prediction_lower_bound, r_value, p_value #potentially useful for subsequent analysis
 
 
-
-### ADX INDICATOR
-
-####components of adx
-#####Directional Movement List Generator
-def DM(listprice):
-    DM_list = []
-    for i in range(listprice):
-        if count > 0:
-            DM = listprice[i] - listprice[i - 1]
-            DM_list.append(DM)
-    return DM_list
-
-#Example for test run: high list
-#HDMList = DM(highlist)
-
-#####True Range
-def TR(listprice1,listprice2): #listprice1,2 have to be the same size.
-    TR_list = [] #further, listprice1 should be the high DM, listprice2 should be the low DM (subject to modification)
-    for i in range(listprice1):
-        if count > 0:
-            Op1 = listprice1[count] - listprice1[count-1]
-            Op2 = listprice2[count] - listprice2[count-1]
-            TR = min(Op1,Op2)
-            TR_list.append(TR)
-    return TR_list
-
-#####Moving Average Types
-"""
-def SmoothAverage(list_len_14): #list length is strictly 14 in this function
-    for count in enumerate(list_len_14,0):
-        heavyterm = 0 #smoothed averages are weighted more heavily towards previous 13 indexes
-        lightterm = list_len_14[13] #current index in whichever metric
-        if count <= 12:
-            heavyterm += list_len_14[count]
-    smoothAverage = (13*heavyterm + lightterm)/14
-    return smoothAverage#returns scalar
-"""
-#suspicion is that exponential moving average is more accurate predictor using ADX than smooth moving average. Subject to modification.
-
-
-######Exponential Moving Average  
-def ExponentialMovingAverage(list):#if applied to ADX, should have the same length the total interval of analysis; gives a specific number
-  EMA_0 = list[0]
-  EMA_y = EMA_0 #for the sake of mathematical coherence
-  N = len(list)
-
-  for i in range(list):
-    EMA_t = list[i]*(2//(N + 1)) + EMA_y(1 - (2//(N + 1)))#see documentation
-    #print(EMA)
-    EMA_y = EMA_t
-  
-  return EMA_t#returns scalar
-
-#Standard Directional Index Measures
-def DI(DM,ATR): #DM, ATR float, not list
-    return 100*DM/ATR
-
-def DX(DI1,DI2,ATR): #DI1,DI2,ATR must be a float, not a list
-    return (100*(abs(DI1 - DI2)//abs(DI1 + DI2)))
-    #alternative versions of DX also could be used. . . projects for later
-
-def DX_initiator(list1,list2):#lists should be same length, n should be integer
-
-  #everything below assembles most of the previous code to give initial dx value, where subsequent adx values are defined by either a smoothed average
-  #or an exponential moving average
-
-  True_Range_Vector = TR(list1,list2)
-  ATR = np.average(True_Range_Vector)
-  DM_high = DM(list1)
-  DM_low = DM(list2)
-  Smoothed_DM_high = ExponentialMovingAverage(DM_high)
-  Smoothed_DM_low = ExponentialMovingAverage(DM_low)
-  high_DI = DI(Smoothed_DM_high,ATR)
-  low_DI = DI(Smoothed_DM_low,ATR)
-  Simple_DX = DX(high_DI,low_DI,ATR)
-  #N = len(list1)
-
-  return Simple_DX #N,Simple_DX
-
-###### ADX functions
-
-### the following loop should apply ADX_initiator to first N entries and then iterate for as many N-length periods as the user demands (provided there is enough data)
+###ADXindictator
 
 def CurrentADX(final,list1,list2,period,NumLargePeriod):
   initial = final - NumLargePeriod*period*14 #possibly some index issues
-  DM_high = DM(list1)
-  DM_low = DM(list2)
+  DM_high = DITools.DM(list1)
+  DM_low = DITools.DM(list2)
   DX_vector = []
   ADX_vector = []
   i = initial
@@ -138,25 +58,26 @@ def CurrentADX(final,list1,list2,period,NumLargePeriod):
 
   #getting initial ADX
 
-  while i < 14*period*NumLargePeriod - 1:#potential indexing issues, to be worked out later
+  while i < 14*period*NumLargePeriod - 2:#potential indexing issues, to be worked out later
     High_list = []
     Low_list = []
     count = 0
+    initialDXVECTORlength = len(DX_vector)
     while count < period:
       High_list.append(DM_high[i + count])
       Low_list.append(DM_low[i + count])
       count += 1
       
-    DX_i = DX_initiator(High_list,Low_list)
+    DX_i = DITools.DX_initiator(High_list,Low_list)
     DX_vector.append(DX_i)#dx vector stuff
     j += 1
+    updatedDXVECTORlength = len(DX_vector)
 
-
-    if i == initial + period*14:#double check for indexing
+    if i == initial + period*14 - 2:#double check for indexing
       currentADX = np.average(DX_vector)
       ADX_vector.append(currentADX)
       k += 1
-    elif (i > initial + period*14) and (i%period == 0):#doublecheck for indexing
+    elif (i > initial + period*14 - 1) and (updatedDXVECTORlength > initialDXVECTORlength):
       prevADX = ADX_vector[int(k) - 1]
       currentADX = DX_i*(2//(N + 1)) + prevADX(1 - (2//(N + 1))) #N = NumLargePeriods813 could possibly be wrong, but this is a relatively easy fix; more problematic would be if this formula is incorrectly implemented
       ADX_vector.append(currentADX)
@@ -165,5 +86,6 @@ def CurrentADX(final,list1,list2,period,NumLargePeriod):
     #changing index by three
     i += count + 1
 
-  return ADX_vector
+  return ADX_vector,ADX_vector[len(ADX_vector)-1]
   #still in progress 1
+
